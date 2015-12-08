94f95b92a61c508b0b031a529231e094e97428a0
Thomas Sibut-Pinote <thomas.sibut-pinote@inria.fr>
2015-12-08 10:12:07 +0100

    whitespace errors

--- ocaml/diffeq.ml
+++ ocaml/diffeq.ml
@@ -3,12 +3,12 @@ open Integrals;;
 open Reification;;
 
 (* a first attempt made only on the exponential function. This will be in the initial commit but will probably disappear right after *)
-let expFixPoint steps intprec init x0 x = 
+let expFixPoint steps intprec init x0 x =
   let (a0,b0) = x0 in
   let expx0 = (rd (exp a0),ru (exp b0)) in
   let rec aux = function
     | 0 -> fun x -> init
-    | k -> 
+    | k ->
       print_int k; print_string " ième étape\n";
       let prediExp = aux (k-1) in
       fun x ->iPlus expx0 (integralIntBounds prediExp intprec (x0) (iPlus x0 x))
@@ -42,10 +42,10 @@ let rec clean = function
   | h1::h2::t -> if h1=h2 then h1::(clean t) else h1::(clean (h2::t))
   | [h] -> [h];;
 
-let rec makeList x y depth = 
+let rec makeList x y depth =
   let rec aux x y = function
   | 0 -> [x]
-  | k -> let m = midpoint (x,y) in 
+  | k -> let m = midpoint (x,y) in
 	 (makeList x m (k-1))@(makeList m y (k-1))
   in clean( List.rev(y::(List.rev(aux x y depth))))
 ;;
@@ -54,19 +54,19 @@ let rec makeList x y depth =
 
 (* a function to get an enclosure without ta *)
 
-let get_enclosure t0 t1 nb phi x0 z0 =  
+let get_enclosure t0 t1 nb phi x0 z0 =
   let l = (makeList t0 t1 nb) in
   let rec aux xlow xhigh tt0 = function
     | [] -> (xlow,xhigh)
-    | tt1::tail -> 
-      let x1 = estimate tt0 tt1 phi (thin xlow) z0 in 
-      let x2 = if xlow=xhigh then x1 else estimate tt0 tt1 phi (thin xhigh) z0 in 
+    | tt1::tail ->
+      let x1 = estimate tt0 tt1 phi (thin xlow) z0 in
+      let x2 = if xlow=xhigh then x1 else estimate tt0 tt1 phi (thin xhigh) z0 in
       aux (fst x1) (snd x2) tt1 (tail)
   in aux (fst x0) (snd x0) t0 l;;
 
 
 
-let applyRec plus mult zero recrel l = 
+let applyRec plus mult zero recrel l =
   let rec head = function
     | ([],[]) -> zero
     | (a::t,fk::frem) -> plus (mult a fk) (head (t,frem))
@@ -82,8 +82,8 @@ let fixpointLin steps intprec x0 y0 valinit recrel =
   let rec aux = function
     | 0 -> valinit (* une liste de fonctions *)
     | k -> let prediF = aux (k-1) in (* une liste de fonctions *)
-	   let aIntegrer = 
-	     (match prediF with 
+	   let aIntegrer =
+	     (match prediF with
 	       | h::t -> let nSub1 = (applyRec plus mult zero recrel prediF) in List.rev (nSub1::(List.rev t))
 	       | _ -> failwith "Error in recurrence in fixpointLin")
 	   in
@@ -98,10 +98,10 @@ let makeIfunFromDiffEq steps intprec x0 y0 valinit recrel x =
 
 
 (* nouvelle tentative de découpage *)
-let estimateSolCut t0  steps intprec recrel (y0 : float list) (valinit: (intervalle -> intervalle) list) (f: intervalle -> intervalle) l =   
+let estimateSolCut t0  steps intprec recrel (y0 : float list) (valinit: (intervalle -> intervalle) list) (f: intervalle -> intervalle) l =
   let rec aux (x: intervalle) (t0:float) = function
     | [] -> x
-    | t2::t -> 
+    | t2::t ->
       let f = fun xx ->  makeIfunFromDiffEq steps intprec (thin t0) [(xx : float)] valinit recrel (t0,t2) in
       let low = f ((fst x)) in
       let up = f ( (snd x)) in
@@ -117,7 +117,7 @@ let fixpointNonLinDim1 steps intprec (x0:intervalle) (y0:intervalle) (valinit : 
 let estimateSolCutNonLin t0 steps intprec (y0: intervalle ) (valinit : (intervalle -> intervalle) ) (f : (intervalle -> intervalle) -> ((intervalle  -> intervalle))) l =
   let rec aux (x:intervalle) (t1:float) = function
     | [] -> x
-    | t2::t -> 
+    | t2::t ->
       let f1 = fun (xx : float) -> fixpointNonLinDim1 steps intprec (thin t1) (thin xx) valinit f in
       let low = f1 (fst x) (t2,t2) in
       let up = f1 (snd x) (t2,t2) in
@@ -126,4 +126,3 @@ let estimateSolCutNonLin t0 steps intprec (y0: intervalle ) (valinit : (interval
       let xnew = (xnew1,xnew2) in
       aux xnew t2 t
   in aux y0 t0 (List.tl l);;
-
